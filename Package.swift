5edc742e7b2a6dc456cbd051bfc2d7435e070f58
Adam Young <me@adam-young.co.uk>
2026-03-16 18:46:38 +0000

    Update GitHub Actions workflow

--- Package.swift
+++ Package.swift
@@ -4,15 +4,17 @@
 import PackageDescription
 
 let package = Package(
+
     name: "PopcornEPG",
+
     platforms: [.macOS(.v13)],
+
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/adamayoung/TMDb.git", from: "17.0.0")
     ],
+
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "PopcornEPG",
             dependencies: [
@@ -21,4 +23,5 @@ let package = Package(
             ]
         )
     ]
+
 )
